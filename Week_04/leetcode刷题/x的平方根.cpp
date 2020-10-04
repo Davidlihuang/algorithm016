9e024ac0ad800f0dd9fbb06751a7852da02aa7a1
huangli <huanglixianhui@163.com>
2020-10-04 17:49:53 +0800

    上传刷题源代码

---
+++ Week_04/leetcode刷题/x的平方根.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0 || x == 1) return x;
+        long left =1, right = x;
+        while(left <= right) {
+             long mid = left + (right-left)/2;
+             if(mid < x/mid) left = mid+1;
+             else if(mid > x/mid) right = mid-1;
+             else if(mid == x/mid) return mid;
+        }
+        return (int)(right);
+    }
+};
---
+++ Week_04/leetcode刷题/柠檬水找零.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five =0, ten =0;
+        for(auto bill:bills) {
+            switch(bill) {
+                case 5: five++; break;
+                case 10: five--; ten++; break;
+                case 20: if(ten > 0) {ten--; five--;} else {five -= 3;} break;
+            }
+            if(five < 0) return false;
+        }
+        return true;
+    }
+};
